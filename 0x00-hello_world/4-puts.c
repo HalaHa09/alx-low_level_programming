5106762b7f8a826ddf7886db3eb4388c1a69734a
HalaHa09 <hala.haouani@gmail.com>
2023-03-16 18:25:29 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
+"include<stdio.h>
 int main(void)
-{
-	int puts(char)
-	{
-		printf("%c\n");
-		return (0);	
-	}
-	puts("Programming is like building a multilingual puzzle");
+{	
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
